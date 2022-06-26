98ea0b7dc7e5ac03d0c0d6b493bc60e2b4ce24c1
polverecosmica <107887007+polverecosmica@users.noreply.github.com>
2022-06-26 23:20:55 +0300

    Update [hw]03_task_string.py

--- Module3/home_work/string/[hw]03_task_string.py
+++ Module3/home_work/string/[hw]03_task_string.py
@@ -1,3 +1,17 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = str(input("enter your text here: "))
+
+text = text.lower()
+conson1 = ("БВГДЖЗЙКЛМНПРСТФХЦЧШЩ")
+conson1 = conson1.lower()
+count_cons = 0
+for symbol in text:
+    count = 0
+    while count < len(conson1):
+        if symbol == conson1[count]:
+            count_cons += 1
+            count += 1
+        else:
+            count += 1
+print(count_cons)
